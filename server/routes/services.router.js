80090ae8b746e538a48979a90753b5cb87ff392b
Roman L <lenshov512@gmail.com>
2021-10-30 11:23:26 +0300

    hz4

--- server/routes/services.router.js
+++ server/routes/services.router.js
@@ -1,30 +1,64 @@
-const express = require('express');
-const { FullService, Service, Component } = require('../db/models');
+const express = require('express')
+const {
+  FullService,
+  Service,
+  Component,
+  FullServiseService,
+} = require('../db/models')
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', async (req, res) => {
   try {
-    const { carModelId, milegeId } = req.query;
-    console.log(req.query);
+    const { carModelId, milegeId } = req.query
+    console.log(req.query)
 
-    const fullService = await FullService.findAll({
-      where: {
-        MilegeId: +milegeId,
-        CarModelId: +carModelId,
-      },
-      raw: true,
-      include: [Service],
-    });
-    console.log(JSON.stringify(fullService, null, 3));
+    const fullService = await FullService.findAll(
+      {
+        // where: {
+        //   MilegeId: +milegeId,
+        //   CarModelId: +carModelId,
+        // },
+        // raw: true,
+        include: [
+          {
+            model: FullServiseService,
+            include: [FullService, Service]
+          },
+          {
+            model: Service,
+            include: {
+              model: FullService,
+              include: {
+                model: FullServiseService,
+                include: [FullService, Service]
+              }
+            }
+          }
+        ]})
+        // include: {model : FullServiseService, include: [FullService, Service]}
+      
+  
+      // {
+      //   model: Service,
+      //   include: {
+      //     model: FullService,
+      //     include: {
+      //       model: FullServiseService,
+      //       include: [FullService, Service],
+      //     },
+      //   },
+      // },
+    
+    // console.log(JSON.stringify(fullService, null, 3));
 
     // const services = await Service.findAll({ raw: true, include: [FullService] });
-
-    res.json({ fullService });
+    console.log(fullService);
+    res.json({ fullService })
   } catch (error) {
-    console.log(error.message);
-    res.status(500).end();
+    console.log(error.message)
+    res.status(500).end()
   }
-});
+})
 
-module.exports = router;
+module.exports = router
