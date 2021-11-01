18b4cd72a088fe0dcbc5149831011c7488c29ff4
Muxaun <1871mg@gmail.com>
2021-11-01 11:28:11 +0300

    mockup 3

--- client/src/components/AddEntryForm/AddEntryForm.jsx
+++ client/src/components/AddEntryForm/AddEntryForm.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { getDayForInput } from '../../utils/getToday';
 import { getMaxDayFromToday } from '../../utils/getMaxDay';
+import styles from './AddEntryForm.module.css';
+import Button from '../UI/Button/Button'
 
 const AddEntryForm = () => {
 const onSubmit = async (event) => {
@@ -8,42 +10,36 @@ const onSubmit = async (event) => {
   const { car, time, date } = event.target;
   const startDate = new Date(`${date.value} ${time.value}`)
   const endDate = new Date(startDate.getTime() + (2 * 60 * 60 * 1000))
-  
-  
+ }
 
-}
-
-  return (    
+  return (
+  	<ul className={styles.addentryform}>
     <form onSubmit={onSubmit}>
-      <h3>Выберите машину</h3>
       <select name="car" id="">
         <option value="car 1">Car 1</option>
       </select>
-      <h3>Выберите дату</h3>
+      <li>выберите дату и время</li>
       <input required name="date" defaultValue={getDayForInput(new Date())} type="date" min={getDayForInput(new Date())} max={getMaxDayFromToday(30)} />
       <input required id="appt-time"defaultValue="09:00:00" list="times" min="09:00:00" max="18:00:00" type="time" name="time" step="1800" />
       <datalist id="times">
-          <option value="09:00:00" />
-          <option value="09:30:00" />
-          <option value="10:00:00" />
-          <option value="10:30:00" />
-          <option value="11:00:00" />
-          <option value="11:30:00" />
-          <option value="12:00:00" />
-          <option value="12:30:00" />
-          <option value="13:00:00" />
-          <option value="13:30:00" />
-          <option value="14:00:00" />
-          <option value="14:30:00" />
-          <option value="15:00:00" />
-          <option value="15:30:00" />
-          <option value="16:00:00" />
-          <option value="16:30:00" />
-          <option value="17:00:00" />
-          <option value="17:30:00" />
+	      <option value="08:00:00" />
+	      <option value="09:00:00" />
+        <option value="10:00:00" />
+        <option value="11:00:00" />
+	      <option value="12:00:00" />
+        <option value="13:00:00" />
+        <option value="14:00:00" />
+        <option value="15:00:00" />
+        <option value="16:00:00" />
+	      <option value="17:00:00" />
+	      <option value="18:00:00" />
+	      <option value="19:00:00" />
+	      <option value="20:00:00" />
+	      <option value="21:00:00" />
       </datalist>
-      <input type="submit" value='Записаться на ТО' />
+      <Button name='ЗАПИСАТЬСЯ' />
     </form>
+	  </ul>
   )
 }
 
