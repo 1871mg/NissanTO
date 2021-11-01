231c4495152235ae31313da3a2e07b937d3ef4ca
Roman L <lenshov512@gmail.com>
2021-11-01 11:05:19 +0300

    view  serviceList

--- client/src/components/UI/SelectComponentsButton/SelectComponentsButton.jsx
+++ client/src/components/UI/SelectComponentsButton/SelectComponentsButton.jsx
@@ -1,25 +1,32 @@
-import React, { useState } from 'react';
-import Select from 'react-select';
+import React, { useState } from 'react'
+import {useSelector} from 'react-redux'
+import Select from 'react-select'
 
 const options = [
   { value: 'components1', label: 'Фильтр воздушный' },
   { value: 'components2', label: 'Масляный фильтр' },
-  { value: 'components3', label: 'Уплотнение пробки корпуса раздаточной коробки' },
-];
+  {
+    value: 'components3',
+    label: 'Уплотнение пробки корпуса раздаточной коробки',
+  },
+]
 
 function SelectComponentsButton() {
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedOption, setSelectedOption] = useState(null)
+  const addComponents = useSelector((state) => state.serviceInfoReducer.components)
 
   return (
     <>
       <Select
         defaultValue={selectedOption}
         onChange={setSelectedOption}
-        options={options}
-        placeholder={<div className="select-placeholder-text">дополнительные запчасти</div>}
+        options={addComponents.map((addComponent) => addComponent = { value: addComponent.id, label: `${addComponent.title}: ${addComponent.price} ₽` })}
+        placeholder={
+          <div className="select-placeholder-text">дополнительные запчасти</div>
+        }
       />
     </>
-  );
+  )
 }
 
-export default SelectComponentsButton;
+export default SelectComponentsButton
