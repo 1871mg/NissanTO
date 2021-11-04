42bf8bea8d9ffcd44cc1a9215e3b5968e3e3aaa1
Andrey <dronrus96@mail.ru>
2021-11-04 10:28:21 +0300

    changed seeders

--- server/db/seeders/2.3_20211031181641-Add_cars.js
+++ server/db/seeders/2.3_20211031181641-Add_cars.js
@@ -1,6 +1,20 @@
 const cars = [
   {
     OwnerId: 1, CarModelId: 1, stateNumber: 'а111аа11', yearIssue: 2020, MilegeId: 1,
+  }, {
+    OwnerId: 2, CarModelId: 5, stateNumber: 'б111бб11', yearIssue: 2020, MilegeId: 1,
+  }, {
+    OwnerId: 3, CarModelId: 10, stateNumber: 'в111вв11', yearIssue: 2020, MilegeId: 1,
+  }, {
+    OwnerId: 4, CarModelId: 15, stateNumber: 'г111гг11', yearIssue: 2020, MilegeId: 1,
+  }, {
+    OwnerId: 5, CarModelId: 20, stateNumber: 'д111дд11', yearIssue: 2020, MilegeId: 1,
+  }, {
+    OwnerId: 6, CarModelId: 25, stateNumber: 'е111ее11', yearIssue: 2020, MilegeId: 1,
+  }, {
+    OwnerId: 7, CarModelId: 30, stateNumber: 'Ж111ЖЖ11', yearIssue: 2020, MilegeId: 1,
+  }, {
+    OwnerId: 8, CarModelId: 35, stateNumber: 'з111зз11', yearIssue: 2020, MilegeId: 1,
   },
 ];
 
